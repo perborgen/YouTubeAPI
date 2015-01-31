23bfe308729b5d7ecd130365c249f98aeafaa72a
perborgen <perhb@hotmail.com>
2015-01-31 20:07:38 +0000

    added files

--- index.js
+++ index.js
@@ -76,6 +76,25 @@ function displayChannelID(channel){
 
 
 
+function handleAPILoaded() {
+  $('#search-button').attr('disabled', false);
+}
+
+// Search for a specified string.
+function search() {
+  var q = $('#query').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet'
+  });
+
+  request.execute(function(response) {
+    var str = JSON.stringify(response.result);
+    $('#search-container').html('<pre>' + str + '</pre>');
+  });
+}
+
+
 
 
 
